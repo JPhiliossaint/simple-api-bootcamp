353773749159ef74049c659e477e563f166b8fde
james philiossaint <yourGitHub@email.comjamesphiliossaint>
2022-05-07 22:50:09 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.querySelector('button').addEventListener('click', ghibli)
+
+function ghibli(){
+    let ghibVal = document.querySelector('input').value
+    let url = `https://pokeapi.co/api/v2/pokemon/${ghibVal}`
+
+    fetch(url) 
+    .then(res => res.json()) // parse response as JSON 
+    .then(data => { 
+      console.log(data) 
+      document.querySelector('img').src = data.sprites.front_default
+      document.querySelector('h2').innerText = data.name
+    }) 
+    .catch(err => { 
+        console.log(`error ${err}`) 
+    }); 
+   
+}
+
+// This website returns an array of objects
+// I would like the user's input to loop through the objects until 
+// it finds one with a matching title property.
+//THen display things about the object
